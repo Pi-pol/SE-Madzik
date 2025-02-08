864f956b5c241e7b734f2aaf3fe1b6047d00a127
Michał Rajzer <michal.rajzer03@gmail.com>
2025-02-08 21:27:48 +0100

    Added gassian

--- madzik/processing.py
+++ madzik/processing.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class PLUMER:
-    def __init__(self, opts: dict[str, any]):
+    def __init__(self, opts):
         self.opts = opts
         self._create_model()
 
@@ -40,28 +40,29 @@ class PLUMER:
 
 
 class GASSIAN:
-    # This is a placeholder for the real model which will do per pixel evaluation
-
-    def __init__(self, opts: dict[str, any]):
+    def __init__(self, opts):
         self.opts = opts
+        self._create_model()
 
     def _create_model(self):
         self.model = keras.Sequential()
-        self.model.add(layers.Input(shape=(512, 512, 16)))
-        self.model.add(layers.Conv2D(
-            32, (3, 3), activation='relu'))
-        self.model.add(layers.Conv2D(
-            32, (3, 3), activation='relu'))
-        self.model.add(layers.Conv2D(
-            32, (3, 3), activation='relu'))
-        self.model.add(layers.Flatten())
+        self.model.add(layers.Input(shape=(1, 1, 16)))
+        self.model.add(layers.Dense(12, activation='relu'))
+        self.model.add(layers.Dense(6, activation='relu'))
+        self.model.add(layers.Dense(1, activation='sigmoid'))
+        self.model.compile(optimizer='adam',
+                           loss=tf.keras.losses.BinaryCrossentropy(),
+                           metrics=['accuracy'])
+        return self.model
 
     def preprocess(self, img: np.ndarray):
         return img
 
-    def parse_image(self, img: np.ndarray):
+    def load_weights(self, path: str):
+        self.model.load_weights(path)
+
+    def parse_whole_image(self, img: np.ndarray):
+        preprocessed = self.preprocess(img)
+        # Predict for each pixel without using loops
+        np.vectorize(self.model.predict)(preprocessed)
         return img
-
-
-plum = PLUMER("opts")
-print("PLUMER loaded")
---
+++ train_with_slurm.py
@@ -0,0 +1,16 @@
+
+from hostlist import expand_hostlist  # pip install python-hostlist
+import os
+import tensorflow as tf
+import logging
+
+task_index = int(os.environ['SLURM_PROCID'])
+n_tasks = int(os.environ['SLURM_NPROCS'])
+tf_hostlist = [("%s:22222" % host) for host in
+               expand_hostlist(os.environ['SLURM_NODELIST'])]
+cluster = tf.train.ClusterSpec({"worker": tf_hostlist})
+server = tf.distribute.Server(
+    cluster.as_cluster_def(), job_name="MADZIK_TRAIN", task_index=task_index)
+
+# This is the code that will be run on each worker
+logging.log(f"Starting worker {task_index}/{n_tasks}")
